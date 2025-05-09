9593d027e41fec317db3d6245658fd6fe1d7ab3c
Axell98 <axellvalle98@gmail.com>
2025-05-08 22:51:15 -0500

    Add modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  modules: ['@nuxt/eslint', '@pinia/nuxt', '@vueuse/nuxt'],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
 })
